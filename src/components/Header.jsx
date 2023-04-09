f6718c8fd1d88670d8d864d99183defd683d7e69
hasanisokay <hasanisokay@gmail.com>
2023-04-10 00:59:08 +0600

    nav responsive done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-
+import { Bars3BottomLeftIcon, XMarkIcon } from '@heroicons/react/24/solid'
 const Header = () => {
+    const [isMenuOpen, setisMenuOpen] = useState(false)
     return (
-        <div className='flex justify-between'>
-            <Link to={'/'} className='text-2xl font-bold'>WorkForce</Link>
-            <div className='ash-p-tag flex gap-8'>
-                <NavLink to={'statistics'}> Statistics</NavLink>
-                <NavLink to={'applied-jobs'}> Applied Jobs</NavLink>
-                <NavLink to={'blog'}> Blog</NavLink>
+        <>
+            <div className='lg:flex justify-between hidden'>
+                <Link to={'/'} className='text-2xl font-bold'>WorkForce</Link>
+                <div className='ash-p-tag flex gap-8'>
+                    <NavLink to={'statistics'}> Statistics</NavLink>
+                    <NavLink to={'applied-jobs'}> Applied Jobs</NavLink>
+                    <NavLink to={'blog'}> Blog</NavLink>
+                </div>
+                <button className="btn btn-success">Start Applying</button>
             </div>
-            <button className="btn btn-success">Start Applying</button>
-        </div>
+            <div className='lg:hidden'>
+                {
+                    !isMenuOpen && (
+                        <button onClick={() => setisMenuOpen(true)}>
+                            <Bars3BottomLeftIcon className='w-10 h-10'></Bars3BottomLeftIcon>
+                        </button>
+                    )
+                }
+                {isMenuOpen && (
+                    <div onClick={() => setisMenuOpen(false)} className='flex flex-col items-center'>
+                        <button> <XMarkIcon className='w-10 h-10'></XMarkIcon> </button>
+                        <div className='flex flex-col gap-4'>
+                            <Link to={'/'} className='text-2xl font-bold text-yellow-700'>WorkForce</Link>
+                            <div className='ash-p-tag space-y-4 flex flex-col'>
+                                <NavLink to={'statistics'}> Statistics</NavLink>
+                                <NavLink to={'applied-jobs'}> Applied Jobs</NavLink>
+                                <NavLink to={'blog'}> Blog</NavLink>
+                            </div>
+                            <button className="btn btn-success text-xs">Start Applying</button>
+                        </div>
+
+
+
+                    </div>
+                )}
+            </div>
+        </>
     );
 };
 
