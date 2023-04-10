40107ef68c4a3ec8f252f722f6a4aa8da275f79a
hasanisokay <hasanisokay@gmail.com>
2023-04-11 00:15:44 +0600

    added see all jobs button onclick in home

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Job = () => {
+const Job = ({jobData}) => {
+    const {id, logo, jobTitle, companyName, jobType, location, fulltimeOrParttime, salary } = jobData
     return (
-        <div>
-            
+        <div className='m-6'>
+            <img src={logo} alt="" />
+            <h1>{jobTitle}</h1>
+            <h2>{companyName}</h2>
+            <div className=""><button>{jobType}</button>
+            <button>{fulltimeOrParttime}</button></div>
         </div>
     );
 };
--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Job from './Job';
 
-const Jobs = () => {
+const Jobs = ({ jobsData }) => {
+    const slicedData = jobsData.slice(0, 4)
+
+    const [initialJobsData, setinitialJobsData] = useState(slicedData)
+    const [expand, setExpand] = useState(false)
+    const handleSeeAllJobs = (isExpanded) => {
+        setinitialJobsData(jobsData)
+        setExpand(true)
+
+    }
+    console.log(initialJobsData);
+
+
+    
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className='grid grid-cols-2 mx-8'>
+                {
+                    initialJobsData.map(jobData => <Job jobData={jobData} key={jobData.id}></Job>)
+                }
+
+            </div>
+            {
+                !expand && (<span className='flex justify-center'>
+                <button className="btn btn-success" onClick={()=>handleSeeAllJobs(true)}>See All Jobs</button>
+            </span>)
+            }
+        </>
     );
 };
 
